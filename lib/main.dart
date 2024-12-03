61dc31d461111c2642aeff20eff81dddbc0907b4
Kauandlr <kauanlara123@gmail.com>
2024-12-03 17:40:59 -0300

    Chamada da API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,97 @@
-import 'package:bibletech/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-main() {
-  runApp(AppWidget());
+const String apiKey = 'AIzaSyC6wwd2Z5FdeulqzO9kjzEBD9F0fOaAOTs';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  String _searchQuery = '';
+  List<dynamic> _books = [];
+
+  Future<void> fetchBooks(String query) async {
+    final response = await http.get(
+      Uri.parse(
+          'https://www.googleapis.com/books/v1/volumes?q=$query&key=$apiKey'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        _books = data['items'];
+      });
+    } else {
+      // Tratar erro
+      setState(() {
+        _books = [];
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Input de pesquisa
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Pesquise pelo livro',
+                suffixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
+              onSubmitted: (value) {
+                fetchBooks(value);
+              },
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _books.length,
+                itemBuilder: (context, index) {
+                  final book = _books[index]['volumeInfo'];
+                  return ListTile(
+                    title: Text(book['title']),
+                    subtitle: Text(book['authors']?.join(', ') ?? 'No authors'),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
